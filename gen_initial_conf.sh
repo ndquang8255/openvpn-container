2e0379357d4e9eba1d601161308dd97c3d190b7e
Quang Nguyen Dang <ndquang8255@gmail.com>
2021-05-29 01:26:40 +0700

    Update initial script to create ovpn data folder automatically

--- gen_initial_conf.sh
+++ gen_initial_conf.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 exe_path=$(pwd)
+mkdir $exe_path/data/conf
+chmod -r 777 $exe_path/data/conf
 docker run -v $exe_path/data/conf:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://$1
 docker run -v $exe_path/data/conf:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
